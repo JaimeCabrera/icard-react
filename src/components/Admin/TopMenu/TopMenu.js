9fac3a862f037c571c9c2efc8079066493d9ec9f
JaimeCabrera <jycabrerap@unl.edu.ec>
2022-11-20 18:42:26 -0500

    add user login interface

--- src/components/Admin/TopMenu/TopMenu.js
+++ src/components/Admin/TopMenu/TopMenu.js
@@ -1,5 +1,22 @@
-import React from "react";
+import { Icon, Menu } from "semantic-ui-react";
 import "./TopMenu.scss";
+import { useAuth } from "../../../hooks/useAuth";
+
 export const TopMenu = () => {
-  return <div>TopMenu</div>;
+  const { auth, logout } = useAuth();
+  console.log(auth);
+
+  return (
+    <Menu fixed="top" className="top-menu-admin">
+      <Menu.Item className="top-menu-admin__logo">
+        <p>iCardAdmin</p>
+      </Menu.Item>
+      <Menu.Menu position="right">
+        <Menu.Item>Hola {auth.me.username}</Menu.Item>
+        <Menu.Item onClick={logout}>
+          <Icon name="sign out" />
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  );
 };
